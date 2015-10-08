9108b3b6b74a1ac2efea686464c398e1adcc7040
Niklas Baudy <niklas.baudy@vanniktech.de>
2015-10-08 20:01:35 +0200

    Add one more test

--- src/test/groovy/com/vanniktech/android/javadoc/GenerationTest.groovy
+++ src/test/groovy/com/vanniktech/android/javadoc/GenerationTest.groovy
@@ -1,5 +1,7 @@
 package com.vanniktech.android.javadoc
 
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.ExpectedException
@@ -14,5 +16,15 @@ class GenerationTest {
         expectedException.expectMessage("Project is not an Android project")
 
         new Generation().apply(null)
+    }
+
+    @Test
+    public void testNotAndroidProject() {
+        final Project project = ProjectBuilder.builder().build();
+
+        expectedException.expect(UnsupportedOperationException.class)
+        expectedException.expectMessage("Project is not an Android project")
+
+        new Generation().apply(project)
     }
 }
